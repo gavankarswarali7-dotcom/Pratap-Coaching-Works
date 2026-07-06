56aa3563430e08a92d41004d016f8fb25e32b9c1
Swarali Gavankar <gavankarswarali7@gmail.com>
2026-07-06 17:41:49 +0530

    Updated homepage and services

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
